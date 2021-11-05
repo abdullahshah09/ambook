08b370e2a62cc795b2335b1e913120480fc61909
abdullahshah09 <abdullahshah09@outlook.com>
2021-11-05 15:17:03 +0000

    previous commit

--- ambook/src/App.js
+++ ambook/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,28 +20,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
-=======
-import React from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
-import header from './components/header/header.components';
-// import Page1 from './Page1';
-// import Page2 from './Page2';
-// import Page3 from './Page3';
- 
-class App extends React.Component {
-render(){
-return(
-<Router >
-<div>
-<Route path="/page3" component={header} />
-{/* <Route path="/Page1" component={Page1} /> */}
-{/* <Route path="/Page2" component={Page2} /> */}
-{/* <Route path="/Page3" component={Page3} /> */}
-</div>
-</Router>
-)
->>>>>>> d31ad16 (the commit1)
+  )
 }
-
-export default App;
