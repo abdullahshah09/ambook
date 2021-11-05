4388b80134e5a983675365797f20ad7ef87f5232
gmh464 <gaominghui464@gmail.com>
2021-11-05 13:05:44 +0000

    working on header-compoment -m

--- ambook/src/App.js
+++ ambook/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
+import header from './components/header/header.components';
+// import Page1 from './Page1';
+// import Page2 from './Page2';
+// import Page3 from './Page3';
+ 
+class App extends React.Component {
+render(){
+return(
+<Router >
+<div>
+<Route path="/" component={header} />
+{/* <Route path="/Page1" component={Page1} /> */}
+{/* <Route path="/Page2" component={Page2} /> */}
+{/* <Route path="/Page3" component={Page3} /> */}
+</div>
+</Router>
+)
 }
-
+}
 export default App;
